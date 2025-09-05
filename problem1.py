242ebae39328a6f9fc034f5671de36e1f137f9d5
ViperEkura <3081035982@qq.com>
2025-09-05 10:37:56 +0800

    feat(data_util): 添加正态性检验功能并更新数据预处理

--- problem1.py
+++ problem1.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from utils.data_uitl import preprocess
+from utils.data_uitl import preprocess, shapiro_test
 from utils.plot_util import plot_distribution, plot_boxplot
 
 def plot(df, safe_path = 'plots'):
@@ -15,5 +15,18 @@ def plot(df, safe_path = 'plots'):
 if __name__ == "__main__":
     df = pd.read_excel('附件.xlsx', sheet_name=0)
     df = preprocess(df)
+    
     plot(df)
+    
+    selectd_col = ['年龄', '孕妇BMI', '原始读段数','唯一比对的读段数', '被过滤掉读段数的比例', '在参考基因组上比对的比例', '重复读段的比例', 
+                    'GC含量', 'X染色体浓度', 'Y染色体浓度', 'X染色体的Z值', 'Y染色体的Z值', 
+                    '13号染色体的Z值', '18号染色体的Z值', "21号染色体的Z值", 
+                    '13号染色体的GC含量', '18号染色体的GC含量', '21号染色体的GC含量']
+    
+    shapiro_test_res = shapiro_test(df, selectd_col)
+    shapiro_test_res.to_excel('shapiro_test_res.xlsx')
+    
+    
+    
+    
     
--- utils/data_uitl.py
+++ utils/data_uitl.py
@@ -2,6 +2,10 @@ import pandas as pd
 import numpy as np
 import re
 
+from scipy.stats import shapiro
+from typing import List
+
+
 def calcu_week(week_str: str) -> float:
     if pd.isna(week_str) or not isinstance(week_str, str):
         return np.nan
@@ -28,9 +32,26 @@ def calcu_fetus_health(health_info: str):
 
 def preprocess(df: pd.DataFrame) -> pd.DataFrame:
     """数据预处理"""
+    df.columns = df.columns.str.strip()
+    df = df.apply(lambda x: x.str.strip() if x.dtype == "object" else x)
+    
     df.drop(columns=["检测日期", "末次月经"], inplace=True)
     df["检测孕周"] = df["检测孕周"].apply(calcu_week)
     df["IVF妊娠"] = df["IVF妊娠"].apply(calcu_pregnancy_type)
     df["胎儿是否健康"] = df["胎儿是否健康"].apply(calcu_fetus_health)
     
     return df
+
+def shapiro_test(df: pd.DataFrame, columns: List[str]):
+    results = []
+    for col in columns:
+        clean_data = df[col].dropna()
+        
+        if len(clean_data) < 3:
+            results.append({'column': col, 'w': np.nan, 'p': np.nan})
+            continue
+        
+        w, p = shapiro(clean_data)
+        results.append({'column': col, 'w': np.around(w, 3), 'p': np.around(p, 3)})
+    
+    return pd.DataFrame(results)
