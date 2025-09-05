5a9a564bd7533193117b1f647791eed5815055eb
ViperEkura <3081035982@qq.com>
2025-09-06 04:18:19 +0800

    feat(problem2):  修改代码以支持 2n+1 编码长度个体表示

--- problem2.py
+++ problem2.py
@@ -7,59 +7,90 @@ def get_params(df: pd.DataFrame):
     over_week_df = calcu_first_over_week(df, "Y染色体浓度", 0.04)
     over_week_df = over_week_df.sort_values("孕妇BMI")
     y, t, bmi = over_week_df["Y染色体浓度"].values, over_week_df["检测孕周"].values, over_week_df["孕妇BMI"].values
-    N = len(df)
-    
-    return N, y, t, bmi
+    return y, t, bmi
 
-def calcu_N(ind: np.ndarray, bmi: np.ndarray) -> np.ndarray:
+def calcu_Ni(ind: np.ndarray, bmi: np.ndarray) -> np.ndarray:
     """
-    input: shape ind(n + 1), bmi(N), y(N)
+    input: shape ind(2n + 1), bmi(N), y(N)
     output: shape Ni(n)
     """
-    n = len(ind) - 1
+    n_seg = (np.size(ind) - 1) // 2
+    bmi_div = ind[:n_seg + 1]
+    Ni = np.zeros(n_seg)
     
-    Ni = np.zeros(n)
-    for i in range(n):
-        start_bmi = ind[i]
-        end_bmi = ind[i + 1]
+    for i in range(n_seg):
+        start_bmi = bmi_div[i]
+        end_bmi = bmi_div[i + 1]
         mask = np.where((bmi >= start_bmi) & (bmi < end_bmi), 1, 0)
         Ni[i] = np.sum(mask)
     
     return Ni
 
-def calcu_P(ind: np.ndarray, bmi: np.ndarray, y) -> float:
+def calcu_Pi(ind: np.ndarray, bmi: np.ndarray, y) -> np.ndarray:
     """
-    input shape: ind(n + 1), bmi(N)
+    input shape: ind(2n + 1), bmi(N)
     output: P (float)
     """
-    n = len(ind) - 1
-    N = len(bmi)
-    P = np.zeros(n)
+    n_seg = (np.size(ind) - 1) // 2
+    Pi = np.zeros(n_seg)
+    bmi_div = ind[:n_seg + 1]
     
-    for i in range(n):
-        start_bmi = ind[i]
-        end_bmi = ind[i + 1]
+    for i in range(n_seg):
+        start_bmi = bmi_div[i]
+        end_bmi = bmi_div[i + 1]
         index_mask = np.where((bmi >= start_bmi) & (bmi < end_bmi))
-        P[i] = np.sum() = np.sum(y[index_mask] > 0.04)
+        Pi[i] = np.sum(y[index_mask] > 0.04)
     
-    P = P / N
+    Pi = Pi / np.size(y)
     
-    return P
+    return Pi
 
-def init_func(bmi:np.ndarray, n_feature: int):
+def valid_func(ind: np.ndarray, bmi: np.ndarray, y: np.ndarray):
+    Pi = calcu_Pi(ind, bmi, y)
+    Ni = calcu_Ni(ind, bmi)
+    n_seg = (np.size(ind) - 1) // 2
+    t = ind[n_seg + 1:]
+
+    if np.any(Pi < 0.95):
+        return False
+    
+    if np.any(Ni <= 25):
+        return False
+    
+    if np.any(t < 10) or np.any(t > 25):
+        return False
+    
+    return True
+
+def init_func(bmi:np.ndarray, y: np.ndarray, n_seg: int):
+    """
+    ind[0: n_seg + 1] -> bmi_div
+    ind[n_seg + 1: 2n_seg + 1] -> t
+    """
     bmi_min, bmi_max = np.min(bmi), np.max(bmi)
-    population = np.random.uniform(bmi_min, bmi_max, (n_feature,))
-    population = np.sort(population, axis=-1)
-    
-    return population
+    bmi_div = np.random.uniform(bmi_min, bmi_max, (n_seg + 1))
+    bmi_div = np.sort(bmi_div, axis=-1)
+    bmi_div[0], bmi_div[-1] = np.min(bmi), np.max(bmi)
 
-def valid_func(ind: np.ndarray, bmi: np.ndarray, y: np.ndarray, n_feature: int):
-    pass
+    t = np.random.uniform(10, 25, (n_seg))
+    ind = np.concatenate([bmi_div, t])
+    
+    while not valid_func(ind, bmi, y):
+        bmi_div = np.random.uniform(bmi_min, bmi_max, (n_seg + 1))
+        bmi_div = np.sort(bmi_div, axis=-1)
+        bmi_div[0], bmi_div[-1] = np.min(bmi), np.max(bmi)
+
+        t = np.random.uniform(10, 25, (n_seg))
+        ind = np.concatenate([bmi_div, t])
+    
+    return ind
+
+
 
 
 if __name__ == '__main__':
     df = preprocess(pd.read_excel('附件.xlsx', sheet_name=0))
-    N, y, t, bmi = get_params(df)
-    bmi_min, bmi_max = np.min(bmi), np.max(bmi)
+    y, t, bmi = get_params(df)
+    init_sol = init_func(bmi, y, n_seg=10)
+
     
-    model = GeneticAlgorithm()
