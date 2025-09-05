656b53a2b4720a2cba30054a2ddd96c7967f0865
ViperEkura <3081035982@qq.com>
2025-09-05 13:12:47 +0800

    refactor(problem1): 修正变量名并调整代码格式

--- problem1.py
+++ problem1.py
@@ -19,17 +19,17 @@ def analyze_data(df: pd.DataFrame):
     plot_boxplot(df, ['13号染色体的Z值', '18号染色体的Z值', "21号染色体的Z值"], safe_path=plot_save_path)
     plot_boxplot(df, ['13号染色体的GC含量', '18号染色体的GC含量', '21号染色体的GC含量'], safe_path=plot_save_path)
     
-    selectd_col = ['年龄', '孕妇BMI', '原始读段数','唯一比对的读段数', '被过滤掉读段数的比例', '在参考基因组上比对的比例', '重复读段的比例', 
+    selected_col = ['年龄', '孕妇BMI', '原始读段数','唯一比对的读段数', '被过滤掉读段数的比例', '在参考基因组上比对的比例', '重复读段的比例', 
                     'GC含量', 'X染色体浓度', 'Y染色体浓度', 'X染色体的Z值', 'Y染色体的Z值', 
                     '13号染色体的Z值', '18号染色体的Z值', "21号染色体的Z值", 
                     '13号染色体的GC含量', '18号染色体的GC含量', '21号染色体的GC含量']
     
-    shapiro_test_res = shapiro_test(df, selectd_col)
+    shapiro_test_res = shapiro_test(df, selected_col)
     shapiro_test_res.to_excel(os.path.join(table_save_path,'shapiro_test_res.xlsx'))
 
-    selectd_col = ["检测孕周"] + selectd_col
-    plot_spearman_heatmap(df, column_names=selectd_col, safe_path=plot_save_path)
-    spearman_test_res = spearman_test(df[selectd_col], 'Y染色体浓度')
+    selected_col = ["检测孕周"] + selected_col
+    plot_spearman_heatmap(df, column_names=selected_col, safe_path=plot_save_path)
+    spearman_test_res = spearman_test(df[selected_col], 'Y染色体浓度')
     spearman_test_res.to_excel(os.path.join(table_save_path,'spearman_test_res.xlsx'))
 
 
