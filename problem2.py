e970611bcf3ab1e77962b73efe0c8102ddb08be2
ViperEkura <3081035982@qq.com>
2025-09-06 04:38:15 +0800

    refactor(problem2.py): 重构代码并移除未使用的函数

--- problem2.py
+++ problem2.py
@@ -26,34 +26,11 @@ def calcu_Ni(ind: np.ndarray, bmi: np.ndarray) -> np.ndarray:
     
     return Ni
 
-def calcu_Pi(ind: np.ndarray, bmi: np.ndarray, y) -> np.ndarray:
-    """
-    input shape: ind(2n + 1), bmi(N)
-    output: P (float)
-    """
-    n_seg = (np.size(ind) - 1) // 2
-    Pi = np.zeros(n_seg)
-    bmi_div = ind[:n_seg + 1]
-    
-    for i in range(n_seg):
-        start_bmi = bmi_div[i]
-        end_bmi = bmi_div[i + 1]
-        index_mask = np.where((bmi >= start_bmi) & (bmi < end_bmi))
-        Pi[i] = np.sum(y[index_mask] > 0.04)
-    
-    Pi = Pi / np.size(y)
-    
-    return Pi
-
-def valid_func(ind: np.ndarray, bmi: np.ndarray, y: np.ndarray):
-    Pi = calcu_Pi(ind, bmi, y)
+def valid_func(ind: np.ndarray, bmi: np.ndarray):
     Ni = calcu_Ni(ind, bmi)
     n_seg = (np.size(ind) - 1) // 2
     t = ind[n_seg + 1:]
 
-    if np.any(Pi < 0.95):
-        return False
-    
     if np.any(Ni <= 25):
         return False
     
@@ -62,7 +39,7 @@ def valid_func(ind: np.ndarray, bmi: np.ndarray, y: np.ndarray):
     
     return True
 
-def init_func(bmi:np.ndarray, y: np.ndarray, n_seg: int):
+def init_func(bmi:np.ndarray, n_seg: int):
     """
     ind[0: n_seg + 1] -> bmi_div
     ind[n_seg + 1: 2n_seg + 1] -> t
@@ -75,7 +52,7 @@ def init_func(bmi:np.ndarray, y: np.ndarray, n_seg: int):
     t = np.random.uniform(10, 25, (n_seg))
     ind = np.concatenate([bmi_div, t])
     
-    while not valid_func(ind, bmi, y):
+    while not valid_func(ind, bmi):
         bmi_div = np.random.uniform(bmi_min, bmi_max, (n_seg + 1))
         bmi_div = np.sort(bmi_div, axis=-1)
         bmi_div[0], bmi_div[-1] = np.min(bmi), np.max(bmi)
@@ -85,12 +62,13 @@ def init_func(bmi:np.ndarray, y: np.ndarray, n_seg: int):
     
     return ind
 
+def mate_func(population):
+    pass
 
 
 
 if __name__ == '__main__':
     df = preprocess(pd.read_excel('附件.xlsx', sheet_name=0))
     y, t, bmi = get_params(df)
-    init_sol = init_func(bmi, y, n_seg=10)
-
-    
+    sol = init_func(bmi=bmi, n_seg=5)
+    print(valid_func(sol, bmi))
