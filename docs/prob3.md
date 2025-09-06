8363744332e111704f1f8672ccb4909291875066
ViperEkura <3081035982@qq.com>
2025-09-06 23:13:01 +0800

    docs(prob2, prob3)： 增加问题三的数学模型描述

--- docs/prob3.md
+++ docs/prob3.md
@@ -1,18 +1,18 @@
 
-
 $$
 \begin{aligned}
-& \min P = \sum_{i=1}^{n} w_i g(T_i) \\
+& \min Z = \sum_{i=1}^{n} w_i (h(T_i) + R(a_i) + R(b_i, c_i, d_i))\\
 & \text{s.t.} 
 \begin{cases} 
 & (1)\quad b_0 < b_1 < b_2 < \cdots < b_{n-1} < b_n \\
 & (2)\quad 10 \leq T_i \leq 25, \quad \forall i = 1,2,\ldots,n \\
 & (3)\quad N_i > 25, \quad \forall i = 1,2,\ldots,n \\
 & (4)\quad P(y > 0.04| T_i) > 0.9 \\
-& (4)\quad w_i = \frac{N_i}{N}, \quad \forall i = 1,2,\ldots,n \\
-& (5)\quad g(T_i) = T_i - 10
-\quad \forall i = 1,2,\ldots,n \\
+& (5)\quad w_i = \frac{N_i}{N}, \quad \forall i = 1,2,\ldots,n \\
 & (6)\quad b_0 = BMI_{min},\quad b_n = BMI_{max} \\
+& (7)\quad h(T_i) =  \alpha T_i \\
+& (8)\quad R_{IVF}(a_i) = \beta \frac{n_{IVF}}{N_i} \\
+& (9)\quad R_{abnormal}(b_i, c_i, d_i) = \gamma \frac{b_i + c_i + d_i}{N_i}
 \end{cases}
 \end{aligned}
 $$
@@ -27,4 +27,5 @@ $$
   - $N_i$ : 第 $i$ 个区间内部的样本数（给定）
   - $N$:  总共样本数（给定）
   - $T_i$: BMI 的分段区间内选定的t检测阈值（给定） 
+  - $\alpha, \beta, \gamma$: 待确定的常量
 
--- problem3.py
+++ problem3.py
@@ -5,10 +5,31 @@ from utils.data_uitl import preprocess, calcu_first_over_week
 from utils.ga import calcu_Ti
 from typing import Dict
 
-def Ra():
+
+def get_init_params():
+    df = preprocess(pd.read_excel('附件.xlsx', sheet_name=0))
+    over_week_df = calcu_first_over_week(df, "Y染色体浓度", 0.04)
+    over_week_df = over_week_df.sort_values("孕妇BMI")
+    return over_week_df
+
+def get_center_and_label(df: pd.DataFrame, n_clusters):
+    df = df[["身高", "孕妇BMI"]]
+    kmeans = KMeans(n_clusters=n_clusters, random_state=42)
+    kmeans.fit(df)
+    return kmeans.cluster_centers_, kmeans.labels_
+
+def split_df(df: pd.DataFrame, label:np.ndarray, n_clusters):
+    splited_df_list = []
+    for n in range(n_clusters):
+        splited_df_list.append(df[label == n])
+    
+    return splited_df_list
+
+
+def R_ivf():
     pass
 
-def Rbcd():
+def R_abnromal():
     pass
 
 
@@ -28,21 +49,9 @@ def valid_func(ind: np.ndarray, params: Dict[str, np.ndarray]):
     pass
 
 
-def get_init_params():
-    df = preprocess(pd.read_excel('附件.xlsx', sheet_name=0))
-    over_week_df = calcu_first_over_week(df, "Y染色体浓度", 0.04)
-    over_week_df = over_week_df.sort_values("孕妇BMI")
-    return over_week_df
-
-
-def get_center_and_label(df: pd.DataFrame):
-    kmeans = KMeans(n_clusters=3, random_state=42)
-    kmeans.fit(df)
-    return kmeans.cluster_centers_, kmeans.labels_
-
-
 if __name__ == "__main__":
+    n_clusters = 3
     
     df = get_init_params()
-    center, label = get_center_and_label(df)
-
+    center, label = get_center_and_label(df, n_clusters)
+    splited_df_list = split_df(df, label, n_clusters)
